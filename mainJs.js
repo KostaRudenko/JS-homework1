71de7e2fdc43ff465192a9b434b0ccd3ad1381b3
KostaRudenko <goglemoogle@gmail.com>
2018-10-30 00:26:26 +0200

    reworked

--- mainJs.js
+++ mainJs.js
@@ -1,36 +1,27 @@
 let blocks = 3;
 
-let switchTime = (n) => {
+function funcSwitch(n) {
     for (let i = 1; i <= blocks; i++) {
-        switch (n) {
-            case i:
-                console.log(i, true);
-                break;
-            default:
-                console.log(i, false);
-                break;
-        }
+        let str = 'switch(n) { case i: console.log(i, true); break; default: console.log(i, false); break;}';
+        eval(str);
     }
-    console.log('---');
-};
+    console.log('----')
+}
 console.time('time switch');
-switchTime(1);
-switchTime(2);
-switchTime(3);
+funcSwitch(1);
+funcSwitch(2);
+funcSwitch(3);
 console.timeEnd('time switch');
 
-let ifTime = (n) => {
+function funcIf(n) {
     for (let i = 1; i <= blocks; i++) {
-        if (n === i) {
-            console.log(i, true);
-        } else {
-            console.log(i, false);
-        }
+        let str = 'if (n === i) { console.log(i, true); } else { console.log(i, false);}';
+        eval(str);
     }
-    console.log('---');
-};
+    console.log('----')
+}
 console.time('time if');
-ifTime(1);
-ifTime(2);
-ifTime(3);
+funcIf(1);
+funcIf(2);
+funcIf(3);
 console.timeEnd('time if');
